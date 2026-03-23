1fde666a858a987dbc654351c80488bbf81e0953
강병용 <byeongyong.kang@vuno.co>
2026-03-23 14:27:58 +0900

    chore(build): replace vite-plugin-dts with tsc declaration-only output

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [
-    dts({
-      outDir: 'dist',
-      insertTypesEntry: true,
-      exclude: ['./src/*', './lib/constants.ts', './lib/main.test.ts']
-    })
-  ],
   build: {
     emptyOutDir: true,
     lib: {
